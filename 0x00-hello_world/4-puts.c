b8e4fdf919ce2150240310d894bc561453cabfd2
joeeyolas <joeeyolas@gmail.com>
2022-06-30 19:19:18 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
-#!/bin/bash
 #include<stdio.h>
+
 /**
  *
  * * main -entry point
@@ -7,8 +7,13 @@
  * * Return: Always 0 (sucess)
  *
  * */
+
 int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+
 }
