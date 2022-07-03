c7ef479c0d98a717735d0b528863896cafecc1f3
joeeyolas <joeeyolas@gmail.com>
2022-07-02 17:48:13 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int n;
-			int i;
-				int h;
+	int i, j;
 
-					for (h = 0 ; h != 8 ; h++)
-							{
-										for (n = h + 1 ; n != 9 ; n++)
-													{
-																	i = n + 1;
-																				do {
-																									putchar('0' + h);
-																													putchar('0' + n);
-																																	putchar('0' + i);
-																																					if (h != 7)
-																																										{
-																																																putchar(',');
-																																																					putchar(32);
-																																																									}
-																																									i++;
-																																												} while (i != 10);
-																						}
-											}
-						putchar('\n');
-							return (0);
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
 }
