4bc755bcb630d22133a883efead7abce167f1e43
joeeyolas <joeeyolas@gmail.com>
2022-07-02 17:57:55 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,14 @@
-#include <stdio.h>
+echo '#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - prints all the numbers of base 16 in lowercase,
- * followed by a new line
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
-char ch;
- for (n = 48; n < 58; n++)
- {
-   putchar(n);
- }
-  for (ch = 'a'; ch <= 'f'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
-}
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}'>101-quote.c
