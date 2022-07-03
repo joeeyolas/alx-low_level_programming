8d2f5597557201a9ea1f596d2959fa57c599bf3c
joeeyolas <joeeyolas@gmail.com>
2022-07-02 17:41:23 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,37 @@
-int c, i;
+#include <stdio.h>
 
-	for (c = '0'; c <= '9'; c++)
+/**
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
 		{
-					for (i = '0'; i <= '9'; i++)
-								{
-												if (c < i)
-																{
-																					putchar(c);
-																									putchar(i);
-
-																													if (c != '8' || (c == '8' && i != '9'))
-																																		{
-																																								putchar(',');
-																																													putchar(' ');
-																																																	}
-																																}
-														}
-						}
-
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
 	putchar('\n');
-
-		return (0);
+	return (0);
+}
