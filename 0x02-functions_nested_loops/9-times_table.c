5ea4f3f6c1faf3c24b1f4d828fffe8d33ad995ff
joeeyolas <joeeyolas@gmail.com>
2022-07-05 19:37:05 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,9 +22,9 @@ void times_table(void)
 
 																						prod = num * mult;
 
-																						if (prod <= 9)
+																				if (prod <= 9)
 																							_putchar(' ');
-																						else
+																				else
 																							_putchar((prod / 10) + '0');
 	
 																						_putchar((prod % 10) + '0');
