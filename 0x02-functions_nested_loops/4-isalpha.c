c59f4d5b430f6b221fbe1f3f257fe407ccc0b0a4
joeeyolas <joeeyolas@gmail.com>
2022-07-05 20:16:54 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,20 +1,20 @@
 /*
- * *File: is the function lowercase or uppercase
- * *Auth: Dr Marcus Imagwe
- * */
+ *File: is the function lowercase or uppercase
+ *Auth: Dr Marcus Imagwe
+ */
 
 #include "main.h"
 /**
- * *_isalpha -check if a character is lowercase or uppercase.
- * *@c: The character to be checked.
- * *
- * * Return: 1 if character is lowercase or uppercase, or otherwise.
- * */
+ *_isalpha -check if a character is lowercase or uppercase.
+ *@c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase or uppercase, or otherwise.
+ */
 
 int _isalpha(int c)
 {
-		if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-					return (1);
-			else
-						return (0);
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
 }
